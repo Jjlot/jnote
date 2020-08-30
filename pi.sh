218825f61e65e757dd7ced867393063165607ad5
Jjlot <jingran84@163.com>
2020-08-30 08:26:43 -0400

    Add pi.sh

---
+++ pi.sh
@@ -0,0 +1,11 @@
+mkdir /home/pi/Desktop/nfs/
+
+sudo mount -t nfs 10.0.0.102:/media/slot2_3t/media /home/pi/Desktop/nfs
+
+git clone https://github.com/Jjlot/jnote.git /home/pi/Desktop
+
+
+# open ssh
+
+# open vnc
+
