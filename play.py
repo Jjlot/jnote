43049a7aa5e8e90707ae1e19250e73d56e513bc7
Jjlot <jingran84@163.com>
2020-09-25 20:39:03 -0400

    Functions

--- play.py
+++ play.py
@@ -3,55 +3,62 @@ import syslog
 import random
 import time
 
-# Mount nfs
-print(" Mounting nfs")
-while os.system("mount | grep Desktop") != 0 :
-    os.system("sudo mount -t nfs 10.0.0.102:/media/slot3_4t/media /home/pi/Desktop/nfs")
-    time.sleep(5)
+def mount_nfs():
+    # Mount nfs
+    print(" Mounting nfs")
+    while os.system("mount | grep Desktop") != 0 :
+        os.system("sudo mount -t nfs 10.0.0.102:/media/slot3_4t/media /home/pi/Desktop/nfs")
+        time.sleep(5)
 
+def main():
 
-prelog = '[Player]'
+    mount_nfs()
 
-syslog.syslog(prelog + 'Start')
+    prelog = '[Player]'
 
-path0 = "/home/pi/Desktop/nfs/"
-# path0 = "/home/media/"
+    syslog.syslog(prelog + 'Start')
 
-continus = ['Anime', 'documentary_file', 'teleplay']
-randplay = ['movie', 'mv', 'video']
-high_freq= ['high_freq']
+    path0 = "/home/pi/Desktop/nfs/"
+    # path0 = "/home/media/"
 
-s = []
-for dirname in continus:
-    print(dirname)
-    g = os.walk(path0 + dirname)
-    for path,dir_list,file_list in g:
-        for dir_name in dir_list:
-            s.append(os.path.join(path, dir_name))
+    continus = ['Anime', 'documentary_file', 'teleplay']
+    randplay = ['movie', 'mv', 'video']
+    high_freq= ['high_freq']
 
-for dirname in randplay:
-    g = os.walk(path0 + dirname)
-    for path,dir_list,file_list in g:
-        for file_name in file_list:
-            s.append(os.path.join(path, file_name))
+    s = []
+    for dirname in continus:
+        print(dirname)
+        g = os.walk(path0 + dirname)
+        for path,dir_list,file_list in g:
+            for dir_name in dir_list:
+                s.append(os.path.join(path, dir_name))
 
-i = 0
-while i < 3:
-    i += 1
-    for dirname in high_freq:
+    for dirname in randplay:
         g = os.walk(path0 + dirname)
         for path,dir_list,file_list in g:
             for file_name in file_list:
                 s.append(os.path.join(path, file_name))
 
+    i = 0
+    while i < 3:
+        i += 1
+        for dirname in high_freq:
+            g = os.walk(path0 + dirname)
+            for path,dir_list,file_list in g:
+                for file_name in file_list:
+                    s.append(os.path.join(path, file_name))
 
-r = random.sample(s, len(s))
 
-for file in r:
-    # cmd = "vlc \"" + file + "\" -f --video-title-show --video-title-position 6 --video-title-timeout 0x7FFFFFFF"
-    cmd = "vlc \"" + file + "\" -f --play-and-exit"
+    r = random.sample(s, len(s))
 
-    print(cmd)
-    syslog.syslog(prelog + cmd)
-    os.system(cmd)
+    for file in r:
+        # cmd = "vlc \"" + file + "\" -f --video-title-show --video-title-position 6 --video-title-timeout 0x7FFFFFFF"
+        cmd = "vlc \"" + file + "\" -f --play-and-exit"
+
+        print(cmd)
+        syslog.syslog(prelog + cmd)
+        os.system(cmd)
+
+if __name__ == '__main__':
+    main()
 
