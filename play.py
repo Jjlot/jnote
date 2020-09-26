563f4a924e50bb3272053672845c359c315f47a9
Jjlot <jingran84@163.com>
2020-09-25 20:36:37 -0400

    Format

--- play.py
+++ play.py
@@ -23,35 +23,35 @@ high_freq= ['high_freq']
 
 s = []
 for dirname in continus:
-	print(dirname)
-	g = os.walk(path0 + dirname)
-	for path,dir_list,file_list in g:
-		for dir_name in dir_list:
-			s.append(os.path.join(path, dir_name))
+    print(dirname)
+    g = os.walk(path0 + dirname)
+    for path,dir_list,file_list in g:
+        for dir_name in dir_list:
+            s.append(os.path.join(path, dir_name))
 
 for dirname in randplay:
-	g = os.walk(path0 + dirname)
-	for path,dir_list,file_list in g:
-		for file_name in file_list:
-			s.append(os.path.join(path, file_name))
+    g = os.walk(path0 + dirname)
+    for path,dir_list,file_list in g:
+        for file_name in file_list:
+            s.append(os.path.join(path, file_name))
 
 i = 0
 while i < 3:
-	i += 1
-	for dirname in high_freq:
-		g = os.walk(path0 + dirname)
-		for path,dir_list,file_list in g:
-			for file_name in file_list:
-				s.append(os.path.join(path, file_name))
+    i += 1
+    for dirname in high_freq:
+        g = os.walk(path0 + dirname)
+        for path,dir_list,file_list in g:
+            for file_name in file_list:
+                s.append(os.path.join(path, file_name))
 
 
 r = random.sample(s, len(s))
 
 for file in r:
-        # cmd = "vlc \"" + file + "\" -f --video-title-show --video-title-position 6 --video-title-timeout 0x7FFFFFFF"
-	cmd = "vlc \"" + file + "\" -f --play-and-exit"
+    # cmd = "vlc \"" + file + "\" -f --video-title-show --video-title-position 6 --video-title-timeout 0x7FFFFFFF"
+    cmd = "vlc \"" + file + "\" -f --play-and-exit"
 
-	print(cmd)
-	syslog.syslog(prelog + cmd)
-	os.system(cmd)	
+    print(cmd)
+    syslog.syslog(prelog + cmd)
+    os.system(cmd)
 
